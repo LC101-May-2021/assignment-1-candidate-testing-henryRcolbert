d655431683f1f3dc0b4bc3e650c0d645e30ca9c3
Foxstar Colbert <>
2021-05-29 18:37:56 +0000

    made the arrays for question and correct answer with the empty array for candidate answer. also switch output using literals

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,18 +5,30 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName;
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
+let questions = [
+  "Who was the first American woman in space? " ,
+  "True or false: 5 kilometer == 5000 meters? ", 
+  "(5 + 3)/2 * 10 = ? ", 
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", 
+  "What is the minimum crew size for the ISS? "
+];
+let correctAnswers = [
+  "Sally Ride", 
+  "true",
+  "40",
+  "Trajectory", 
+  "3"
+];
+let candidateAnswers = [];
 let question;
 let correctAnswer;
 let candidateAnswer;
-let questions;
-let correctAnswers;
-let candidateAnswers;
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-candidateName = input.question("What is your name? "); 
-console.log("candidate's name: " + candidateName); 
+candidateName = input.question("candidate's name: "); 
+
 
 }
 
@@ -29,8 +41,8 @@ candidateAnswers[i] = input.question(`${i+1}) ${questions[i]}`);
   candidateAnswer = candidateAnswers[i] ; 
   correctAnswer = correctAnswers[i];
 
-  console.log("your answer: " + candidateAnswer); 
-  console.log("correct answer: " + correctAnswer) ;
+  console.log(`your answer:  ${candidateAnswer}`); 
+  console.log(`correct answer: ${correctAnswer}`);
   console.log("");
 }
 
